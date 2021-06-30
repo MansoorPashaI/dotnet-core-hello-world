c228be8f752c9080a533e61a737ba40c7c5e681a
MansoorPashaI <65658185+MansoorPashaI@users.noreply.github.com>
2021-06-30 11:34:17 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ namespace HelloWeb
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello world! 11th Change");
+                return context.Response.WriteAsync("Hello world! 12th Change");
             });
         }
     }
